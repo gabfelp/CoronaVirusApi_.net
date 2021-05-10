4c6e09973238fbb76871e56bd5d9b04cd77c8403
gabriel <gabpaivapereira@gmail.com>
2021-05-10 12:55:04 -0300

    first commit

---
+++ Mongo-Dio/Controllers/InfectadoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mongo_Dio.Data.Collections;
+using Mongo_Dio.Models;
+using MongoDB.Driver;
+
+namespace Mongo_Dio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+    }
+}
---
+++ Mongo-Dio/README.md
@@ -0,0 +1,21 @@
+﻿# Simple dotnet API + MongoDB for CoronaVirus tracking
+
+<img src="https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white" /> <img src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white" /> <img alt="MongoDB" src ="https://img.shields.io/badge/MongoDB-%234ea94b.svg?&style=for-the-badge&logo=mongodb&logoColor=white"/>
+
+### CoronaVirus Tracker API built on ASP.NET + C# + MongoDB as a prerequisite of (Digital Innovation + Take) FullStack Bootcamp!
+
+Get method for https://localhost:5001/infectado
+
+Returns a list with all infected people
+
+Post method example for https://localhost:5001/infectado
+
+```json
+{
+	"dataNascimento": "1990-03-01",
+	"sexo": "M",
+	"latitude": -23.5630994,
+	"longitude": -46.6565712
+}
+```
+PS: change appsettings.json with your string connection
